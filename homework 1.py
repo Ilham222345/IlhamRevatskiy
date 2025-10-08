355f18c00b3d05909e51b171904a98495ad5eed8
IlhamRevatskiy <georgiy.revatskiy@icloud.ru>
2025-10-08 22:21:12 +0300

    homework 1

---
+++ homework 1.py
@@ -0,0 +1,21 @@
+a = 7.3
+a = 8.9
+print(a)
+str_1 = "привеn как дела"
+print (str_1)
+str_2 = "это я сейчас пишу программу в програмировании"
+print (str_2)
+b = a
+print (b)
+a = b = c = 0
+a, b, c = 0, 0, 0
+print(a, b, c)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(str_1))
+sale = "hello pain"
+print(sale)
+print(type(sale))
+print(id(sale))
+print(id(a))
