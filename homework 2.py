cc5176b3d1a14153ae914059b3594e046e3c4247
IlhamRevatskiy <georgiy.revatskiy@icloud.ru>
2025-10-19 12:33:13 +0300

    homework 2

---
+++ homework 2.py
@@ -0,0 +1,78 @@
+a = 25
+b = 89
+x = 9.8
+y = 65.7
+print(a, b, x, y)
+print(type(a), type(b), type(x), type(y))
+
+num1 = 105
+num2 = 12.7
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = 10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+
+s1 = "Python"
+s2 = "Программирование"
+print(s1)
+print(s2)
+s1 = """Python
+Программирование"""
+empty = ""
+print(len(empty))
+first_name = "Иван"
+last_name = "Петров"
+full_name = first_name + " " + last_name
+print(full_name)
+
+s = "Возраст: "
+age = 25
+print(s + str(age))
+
+a = "ха "
+print(a * 5)
+
+text = "Привет, мир"
+print(len(text))
+t = ""
+print(len(t))
+
+sentence = "Я изучаю Python"
+print('Python' in sentence)
+print('Java' in sentence)
+
+a = "apple"
+b = "banana"
+print(a == b, a != b, a < b, a > b, a <= b, a >= b)
+print(ord('А'), ord('а'), ord('Я'))
