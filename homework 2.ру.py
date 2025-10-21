4e5bb32bf350d445221d95858862759fc1df2582
IlhamRevatskiy <georgiy.revatskiy@icloud.ru>
2025-10-21 21:29:16 +0300

    homework 3

---
+++ homework 2.ру.py
@@ -0,0 +1,82 @@
+print("Привет, мир")
+print(5, 10, 15)
+print(10+25)
+
+print(1,2,3, sep = "&")
+print("Python", end = " ")
+print("лучший язык")
+
+# x = 3.14
+# y = -8
+# print(f"Координаты точки: {x}, {y}")
+# name = input("Имя")
+# age = input("Возраст")
+# print(f"Имя: {name}, Возраст: {age}")
+
+# name = input("Ваше имя")
+# print(f"Привет {name}")
+
+# a = int(input())
+# b = int(input())
+# print(a+b)
+
+# a = int(input())
+# print(a**2)
+
+print(5 > 3)
+print(10 < 2)
+print(7 == 7)
+print(6 != 8)
+print(4 >= 4)
+print(9 <= 3)
+res = 8 > 12
+print(res)
+print(type(res))
+
+# x = 15
+# print(x % 2 == 0)
+# print(x % 5 == 0)
+# print(x % 3 ==0 and x % 5 == 0)
+
+# y = 4.5
+# print(y >= 1 and y <= 10)
+# print(y >= 0 and y <= 5 or y >=10 and y <=15)
+# print(not(y < 5))
+
+# print(True or False and False)
+# print(not False and True)
+# print(False or not True and True)
+# print(not(10 > 5 or 3 < 1))
+
+# print(bool(0))
+# print(bool(-5))
+# print(bool(3.14))
+# print(bool(""))
+# print(bool("Python"))
+# print(bool(" "))
+
+n = 37
+print(0 < n, n % 2 ==0, n % 3 ==0)
+
+s = "Программирование"
+print(s [0])
+print(len(s))
+print(s [15])
+print(s [2::12])
+perem = s[0:6]
+print(perem)
+perem2 = s[11:16]
+print(perem2)
+perem3 = s[3:6]
+print(perem3)
+print(s[1::2])
+print(s[::-1])
+print(s[0::3])
+print(s[::-2])
+s2 = "п"
+print(s2 + s [1:16])
+
+word = "adcdefgh"
+print(word [2:5])
+print(word [::-1])
+print(word [1:7])
