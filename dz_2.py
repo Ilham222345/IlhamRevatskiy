0e16dbe1a46342674f0a4147841e2998a66b4dbe
IlhamRevatskiy <georgiy.revatskiy@icloud.ru>
2026-01-02 20:22:57 +0300

    dz_2

---
+++ dz_2.py
@@ -0,0 +1,97 @@
+# a = 8
+# b = 3
+# x = 2.5
+# y = -1.7
+# print(a, b, x, y)
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+
+# num1 = 50
+# num2 = 10
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+# a = 10
+# # b = 3
+# # print(a / b)
+# # print(a // b)
+# # print(a % b)
+
+# a = -10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = 10
+# b = -3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# print(5 + 3 *2)
+# print((5+3) * 2)
+# print(10/2 ** 2)
+
+# count = 10
+# count +=5
+# count -=3
+# count *=2
+# count /=4
+# print(count)
+
+# s1 = "Python"
+# s2 = "Программирование"
+# print(s1, s2)
+# s3 = """""
+# Hello
+# Its
+# Me
+# """""
+# print(s3)
+# empty = ""
+# s4 = len(empty)
+# print(s4)
+
+first_name = "Иван"
+last_name = "Петров"
+full_name = first_name + " " + last_name
+print(full_name)
+
+s = "Возраст: "
+age = 25
+full = s + str(age)
+print(full)
+
+laugh = "ха"
+print(laugh * 5)
+print("Такое действие нельяз со строкой делать, число не целое: laugh * 2.5")
+
+text = "Привет, мир!"
+print(len(text))
+text1 = ""
+print(len(text1))
+
+sentence = "Я изучаю Python"
+print("Python" in sentence)
+print("Java" in sentence)
+
+a = "apple"
+b = "banana"
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+print(ord ("A"))
+print(ord ("а"))
+print(ord ("Я"))
