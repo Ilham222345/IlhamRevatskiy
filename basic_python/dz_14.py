0f20f33d159713228da55f4463ddea769c295c61
IlhamRevatskiy <georgiy.revatskiy@icloud.ru>
2026-02-02 14:57:56 +0300

    dz_14

---
+++ basic_python/dz_14.py
@@ -0,0 +1,31 @@
+# from math import sqrt, pow
+# print(sqrt(64))
+# print(pow(5,3))
+
+# import random
+# print(random.randint(1,10))
+# print(random.choice(["Python", "Java", "C++"]))
+
+# import my_module
+# print(my_module.add(3, 5))
+# print(my_module.multiply(4, 6))
+
+# import time
+# start = time.time()
+# time.sleep(2)
+# end = time.time()
+# print(end - start)
+
+# import requests
+# response = requests.get("http://www.baidu.com/")
+# print(response.status_code)
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 50]
+plt.plot(x, y, marker="o")
+plt.title("Пример графика")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
